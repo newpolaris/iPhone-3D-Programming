1ed2d61aabbc1941bc19b96ee022fc42ccfb3209
newpolaris <newpolaris@gmail.com>
2013-11-23 16:16:24 +0900

    BuildProgram, BuildShader function import

--- HelloTriangle/Classes/RenderingEngine2.cpp
+++ HelloTriangle/Classes/RenderingEngine2.cpp
@@ -29,7 +29,6 @@ public:
     void Render() const;
 
 private:
-    GLuint LoadShader ( GLenum type, const char *shaderSrc );
 	GLuint BuildProgram(const char* vertexShaderSource, const char* fragmentShaderSource) const;
 	GLuint BuildShader(const char* source, GLenum shaderType) const;
 
@@ -70,12 +69,32 @@ int RenderingEngine2::Initialize(int width, int height)
    // Bind vPosition to attribute 0   
    // glBindAttribLocation ( programObject, 0, "vPosition" );
 
-
    glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
 
    return TRUE;
 }
 
+void RenderingEngine2::Render() const
+{
+   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f, 
+                           -0.5f, -0.5f, 0.0f,
+                            0.5f, -0.5f, 0.0f };
+      
+   // Set the viewport
+   glViewport ( 0, 0, mWidth, mHeight );
+   
+   // Clear the color buffer
+   glClear ( GL_COLOR_BUFFER_BIT );
+
+   // Use the program object
+   glUseProgram ( m_simpleProgram );
+
+   // Load the vertex data
+   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
+   glEnableVertexAttribArray ( 0 );
+
+   glDrawArrays ( GL_TRIANGLES, 0, 3 );
+}
 
 
 GLuint RenderingEngine2::BuildProgram(const char* vertexShaderSource, const char* fragmentShaderSource) const {
@@ -115,74 +134,5 @@ GLuint RenderingEngine2::BuildShader(const char* source, GLenum shaderType) cons
     }
 
     return shaderHandle;
-}
-
-///
-// Create a shader object, load the shader source, and
-// compile the shader.
-//
-GLuint RenderingEngine2::LoadShader ( GLenum type, const char *shaderSrc )
-{
-   GLuint shader;
-   GLint compiled;
-   
-   // Create the shader object
-   shader = glCreateShader ( type );
-
-   if ( shader == 0 )
-   	return 0;
-
-   // Load the shader source
-   glShaderSource ( shader, 1, &shaderSrc, NULL );
-   
-   // Compile the shader
-   glCompileShader ( shader );
-
-   // Check the compile status
-   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
-
-   if ( !compiled ) 
-   {
-      GLint infoLen = 0;
-
-      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
-      
-      if ( infoLen > 1 )
-      {
-         char* infoLog = (char*)malloc (sizeof(char) * infoLen );
-
-         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
-         esLogMessage ( "Error compiling shader:\n%s\n", infoLog );            
-         
-         free ( infoLog );
-      }
-
-      glDeleteShader ( shader );
-      return 0;
-   }
-
-   return shader;
-
-}
-void RenderingEngine2::Render() const
-{
-   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f, 
-                           -0.5f, -0.5f, 0.0f,
-                            0.5f, -0.5f, 0.0f };
-      
-   // Set the viewport
-   glViewport ( 0, 0, mWidth, mHeight );
-   
-   // Clear the color buffer
-   glClear ( GL_COLOR_BUFFER_BIT );
-
-   // Use the program object
-   glUseProgram ( m_simpleProgram );
-
-   // Load the vertex data
-   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
-   glEnableVertexAttribArray ( 0 );
-
-   glDrawArrays ( GL_TRIANGLES, 0, 3 );
 }
 
