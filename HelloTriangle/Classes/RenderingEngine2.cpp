57e3ecdd04a39c6f641f4465788a40d81a87ef51
newpolaris <newpolaris@gmail.com>
2013-11-24 16:01:28 +0900

    TEST

--- HelloTriangle/Classes/RenderingEngine2.cpp
+++ HelloTriangle/Classes/RenderingEngine2.cpp
@@ -32,6 +32,10 @@ private:
 	GLuint BuildProgram(const char* vertexShaderSource, const char* fragmentShaderSource) const;
 	GLuint BuildShader(const char* source, GLenum shaderType) const;
 
+	// GL의 상태 변화는 알지 못하네?
+	void ApplyRotation(float degrees) const;
+	void ApplyOrtho(float maxX, float maxY) const;
+
     GLuint m_simpleProgram;
 	int mWidth;
 	int mHeight;
@@ -48,11 +52,11 @@ struct Vertex {
 };
 
 const Vertex Vertices[] = {
-    {{-0.5, -0.866},        {1, 1, 0.5f, 1}},
-    {{0.5, -0.866},         {1, 1, 0.5f, 1}},
+    {{-0.5f, -0.866},        {1, 1, 0.5f, 1}},
+    {{0.5f, -0.866},         {1, 1, 0.5f, 1}},
     {{0, 1},                {1, 1, 0.5f, 1}},
-    {{-0.5, -0.866},        {0.5f, 0.5f, 0.5f}},
-    {{0.5, -0.866},         {0.5f, 0.5f, 0.5f}},
+    {{-0.5f, -0.866},        {0.5f, 0.5f, 0.5f}},
+    {{0.5f, -0.866},         {0.5f, 0.5f, 0.5f}},
     {{0, -0.4f},            {0.5f, 0.5f, 0.5f}}
 };
 
@@ -64,41 +68,73 @@ RenderingEngine2::RenderingEngine2()
 
 int RenderingEngine2::Initialize(int width, int height) 
 {
-   mWidth = width;
-   mHeight = height;
+	mWidth = width;
+	mHeight = height;
 
-    //create framebuffer object and attach the colour buffer
-    /*
-    glGenFramebuffers(1, &m_framebuffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_renderbuffer);
-    */
+	// create framebuffer object and attach the colour buffer
+	// glGenFramebuffers(1, &m_framebuffer);
+	// glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+	// glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_renderbuffer);
 
-   // Load the vertex/fragment shaders
-   m_simpleProgram = BuildProgram(SimpleVertexShader, SimpleFragmentShader );
-
-   // Bind vPosition to attribute 0   
-   // glBindAttribLocation ( programObject, 0, "vPosition" );
-
-   glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
-
-   return TRUE;
-}
-
-void RenderingEngine2::Render() const
-{
-	GLfloat vVertices[] = {  0.0f,  0.5f, 
-		-0.5f, -0.5f,
-		0.5f, -0.5f };
+	// Load the vertex/fragment shaders
+	m_simpleProgram = BuildProgram(SimpleVertexShader, SimpleFragmentShader );
 
 	// Set the viewport
 	glViewport ( 0, 0, mWidth, mHeight );
 
+	// glUseProgram(m_simpleProgram);
+
+	// ApplyOrtho(2, 3);
+
+	glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+
+	return TRUE;
+}
+
+void RenderingEngine2::ApplyOrtho(float maxX, float maxY) const
+{
+	float a = 1.0f / maxX;
+	float b = 1.0f / maxY;
+
+	float ortho[16] = {
+		a, 0, 0, 0,
+		0, b, 0, 0,
+		0, 0, -1, 0,
+		0, 0, 0, 1
+	};
+
+	GLint projectionUniform = glGetUniformLocation(m_simpleProgram, 
+												   "Projection");
+
+	glUniformMatrix4fv(projectionUniform, 1, 0, &ortho[0]);
+}
+
+void RenderingEngine2::ApplyRotation(float degrees) const
+{
+	float radians = degrees * 3.14159f / 180.0f;
+	float s =std::sin(radians);
+	float c = std::cos(radians);
+
+	float zRotation[16] = {
+		c, s, 0, 0,
+		-s, c, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	};
+
+	GLint modelviewUniform = glGetUniformLocation(m_simpleProgram,
+												 "ModelView");
+
+	glUniformMatrix4fv(modelviewUniform, 1, 0, &zRotation[0]);
+}
+
+void RenderingEngine2::Render() const
+{
 	// Clear the color buffer
+	// glClearColor(0.5f, 0.5f, 0.5f, 1);
 	glClear ( GL_COLOR_BUFFER_BIT );
 
-	// Use the program object
-	glUseProgram ( m_simpleProgram );
+	// ApplyRotation(25);
 
 	GLuint positionSlot = glGetAttribLocation(m_simpleProgram, "vPosition");
 	GLuint colorSlot = glGetAttribLocation(m_simpleProgram, "SourceColor");
