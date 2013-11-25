8678a21cd0de7426f7d236fc144f0a2222ce5fd0
newpolaris <newpolaris@gmail.com>
2013-11-26 08:24:10 +0900

    misc

--- HelloTriangle/Classes/IRenderingEngine.hpp
+++ HelloTriangle/Classes/IRenderingEngine.hpp
@@ -1,4 +1,5 @@
 #include "esUtil.h"
+#include "Vector.hpp"
 
 typedef struct
 {
@@ -17,12 +18,16 @@ enum DeviceOrientation {
     DeviceOrientationFaceUp,
     DeviceOrientationFaceDown,
 };
+
 // OpenGL ES Renderer interface. Used by GLView class
 struct IRenderingEngine {
 	virtual int Initialize(int width, int height) = 0;
 	virtual void Render() const = 0;
     virtual void UpdateAnimation(float timeStep) = 0;
     virtual void OnRotate(DeviceOrientation orientation) = 0;
+    virtual void OnFingerUp(ivec2 location) = 0;
+    virtual void OnFingerDown(ivec2 location) = 0;
+    virtual void OnFingerMove(ivec2 oldLocation, ivec2 newLocation) = 0;
 	virtual ~IRenderingEngine() {}
 };
 
